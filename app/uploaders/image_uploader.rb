82a9ee5946c6f6940209d914acb82af1066edb06
mihomatsui <mihomatsui.24@gmail.com>
2021-04-02 23:40:11 +0900

    画像ファイル形式のバリデーションを追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,4 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 end
